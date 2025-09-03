f8fbf79d69c4d0352501bb5225d1b9f59b7dfcd2
anthony-murè <tony.mure99@gmail.com>
2025-09-03 17:13:41 +0200

    [add]components(Header.jsx)(Main.jsx-form-post-static)

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-12'>
+          <h1 className='text-center'>Post Form</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Main.jsx
@@ -0,0 +1,51 @@
+
+
+const Main = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h2>Crea un nuovo post</h2>
+        </div>
+       <div className="row">
+        <form>
+          <div className="col-12 my-3">
+            <input 
+             className="form-control"
+              type="text"
+              name="author"
+              placeholder="inserisci l'autore del nuovo post"
+             />
+          </div>
+          <div className="col-12 my-3">
+            <input 
+              className="form-control"
+               type="text"
+               name="title"
+               placeholder="inserisci il titolo del nuovo post"
+            />
+          </div>
+          <div className="col-12 my-3">
+            <input 
+            className="form-control"
+            type="textarea"
+            name="body"
+            placeholder="inserisci il testo del nuovo post"
+            />
+          </div>
+           <select className="form-select">
+            <option value="">scegli un opzione</option>
+            <option value="true">post pubblico</option>
+            <option value="false">post privato</option>
+           </select>
+           <div class="col-12 mt-3">
+             <button class="btn btn-primary" type="submit">invia post</button>
+           </div>
+        </form>
+       </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
